65873ae1d0a2ea71ba65ba4673176bb8cd79a811
Maisha Nawar <maishanawar27@gmail.com>
2021-09-23 11:21:03 -0400

    made card swappable

---
+++ src/utilities/data.ts
@@ -0,0 +1,10 @@
+// https://stackoverflow.com/a/5915122
+/**
+ * Grab a random element from a list
+ * @param items Any kind of list
+ * @returns One randomly chosen element
+ */
+
+export function getRandomElement<T>(items:T[]): T {
+    return items[Math.floor(Math.random()*items.length)];
+}
