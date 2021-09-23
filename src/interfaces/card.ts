0a22b90bbcc6d84859b1ea0c3f2c6b8cdc4c95ac
Maisha Nawar <maishanawar27@gmail.com>
2021-09-23 01:53:35 -0400

    Card interface created

---
+++ src/interfaces/card.ts
@@ -0,0 +1,14 @@
+//export type CardType = 'Easy' | 'Medium' | 'Hard';
+
+export enum CardKind{
+    Easy = "Easy question",
+     Medium = "Medium question",
+      Hard = "Hard question"
+}
+export interface Card{
+    ID:  number
+    Kind: string
+    Prompt: string
+    Answer: string
+}
+
